588711fc1f8d73a3e7fc63afdb99c9339d2e047b
BryanYunche <estudanteporesporte@gmail.com>
2023-02-26 01:51:05 -0300

    Terminar a atividade de laço de repetição em Java

---
+++ Estruturas de Repetição e Arrays em Java/Exercicio_Nome_Idade/src/Exemplo_Nome_Idade.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/*
+Faça um programa que leia dois valores, o primeiro
+ */
+public class Exemplo_Nome_Idade {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+
+    }
+}
