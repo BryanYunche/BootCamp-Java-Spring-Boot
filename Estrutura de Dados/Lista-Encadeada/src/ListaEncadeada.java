d7746a48ccf4fba44f94d86d0ca630d1274d5fb7
BryanYunche <estudanteporesporte@gmail.com>
2023-03-05 14:45:19 -0300

    Implementação da lista encadeada

--- Estrutura de Dados/Lista-Encadeada/src/ListaEncadeada.java
+++ Estrutura de Dados/Lista-Encadeada/src/ListaEncadeada.java
@@ -2,10 +2,14 @@ public class ListaEncadeada<T> {
 
     No<T> referenciaEntrada;
 
+    public ListaEncadeada() {
+    }
+
     public ListaEncadeada(No<T> referenciaEntrada) {
         this.referenciaEntrada = null;
     }
 
+    //Vai adicionar um nó no final da fila
     public void add(T conteudo){
         //Instancia o conteudo do nó de acordo com o valor recebido
         No<T> novoNo = new No<>(conteudo);
@@ -23,7 +27,40 @@ public class ListaEncadeada<T> {
             noAuxiliar.setProximoNo(novoNo);
         }
     }
+    //Retorna o conteúdo do nó
+    public T get(int index){
+        return getNo(index).getConteudo();
+    }
 
+    //Método privado da Classe
+    private No<T> getNo(int index){
+        validaIndice(index);
+        No<T> noAuxiliar = referenciaEntrada;
+        No<T> noRetorno = null;
+
+        for (int i = 0; i <= index; i++){
+            noRetorno = noAuxiliar;
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        return noRetorno;
+    }
+
+    //Método para remoção de nó
+    public T remove(int index){
+        //O nó pivor vai receber a referência do nó que vai ser removido
+        No<T> noPivor = this.getNo(index);
+
+        if (index == 0){
+            referenciaEntrada = noPivor.getProximoNo();
+            return noPivor.getConteudo();
+        }
+        No<T> noAnterior = getNo(index - 1);
+        noAnterior.setProximoNo(noPivor.getProximoNo());
+        //Vai retornar o nó que foi removido
+        return noPivor.getConteudo();
+    }
+
+    //Vai retornar o tamanho da lista
     public int size(){
         int tamanhoLista = 0;
 
@@ -47,7 +84,29 @@ public class ListaEncadeada<T> {
         return tamanhoLista;
     }
 
+    //Vai validar se o indice é maior ou igual ao tamanho da lista
+    private void validaIndice(int index){
+        //Valida se o indice buscado é maior que a lista
+        if (index >= size()){
+            int ultimoIndice = size()-1;
+            throw new IndexOutOfBoundsException("Não existe conteudo no indice " + index + " desta lista, esta lista só vai até " + ultimoIndice + '.' );
+        }
+    }
+
     public boolean isEmpty(){
         return referenciaEntrada == null ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxiliar = referenciaEntrada;
+
+        for (int i = 0; i < this.size(); i++){
+            strRetorno += "[No{conteudo: " + noAuxiliar +"}]-->";
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        strRetorno += "null";
+        return strRetorno;
     }
 }
