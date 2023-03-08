1d4957f3da7ec452da82d772631edfb700c7e983
BryanYunche <estudanteporesporte@gmail.com>
2023-03-08 03:08:22 -0300

    Terminei a lista duplamente encadeada

--- Estrutura de Dados/Lista-Duplamente-Encadeada/src/ListaDuplamenteEncadeada.java
+++ Estrutura de Dados/Lista-Duplamente-Encadeada/src/ListaDuplamenteEncadeada.java
@@ -26,6 +26,63 @@ public class ListaDuplamenteEncadeada<T> {
         tamanhoLista++;
     }
 
+    //Sobrecarga do método add
+    public void add(int index, T elemento){
+        //Pega o nó que tá no indice que eu tô passando
+        NoDuplo<T> noAuxiliar = getNo(index);
+        //Cria um nó a partir do elemento inserido
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        //Isso quer dizer que o novo nó vai ser adcionado imediatamente atrás do indice do nó do parâmentro
+        novoNo.setNoProximo(noAuxiliar);
+
+        //Se esse indice que pegamos NÃO for o ultimo item da lista
+        if (novoNo.getNoProximo() != null){
+            //O nó previo do nó do indice que pegamos, agora é o nó prévio do indice que iremos colocar
+            novoNo.setNoPrevio(noAuxiliar.getNoPrevio());
+            //Agora o nó previo do nó que passamos no indice, será o novo nó que estamos incerindo
+            novoNo.getNoProximo().setNoPrevio(novoNo);
+        }else {
+            //Se for nulo, estamos no ultimo elemento da nossa lista
+            novoNo.setNoPrevio(ultimoNo);
+            //Logo o novo nó vai ser o ultimo nó da lista
+            ultimoNo = novoNo;
+        }
+
+        //Se for o primeiro indice da lista
+        if (index == 0) {
+            primeiroNo = novoNo;
+        }else {
+            novoNo.getNoPrevio().setNoProximo(novoNo);
+        }
+        //Aumenta em um o tamanho da lista
+        tamanhoLista++;
+    }
+
+    //Remove um nó da fila de acordo com o indice
+    public void remove(int index){
+
+        if (index == 0){
+            primeiroNo = primeiroNo.getNoProximo();
+            //Testa se o segundo nó da lista existe
+            if (primeiroNo != null){
+                primeiroNo.setNoPrevio(null);
+            }else {
+                //Se esse não for o primeiro nó da lista
+                NoDuplo<T> noAuxiliar = getNo(index);
+                noAuxiliar.getNoPrevio().setNoProximo(noAuxiliar.getNoProximo());
+                //Testa se eu NÃO estou no ultimo nó
+                if (noAuxiliar != ultimoNo){
+                    noAuxiliar.getNoProximo().setNoPrevio(noAuxiliar.getNoPrevio());
+                }else {
+                    //Se for o ultimo nó
+                    ultimoNo = noAuxiliar;
+                }
+            }
+        }
+        tamanhoLista--;
+    }
+
+    //Método que retorna um nó a partir de um index
     public T get(int index){
         return getNo(index).getConteudo();
     }
@@ -43,5 +100,19 @@ public class ListaDuplamenteEncadeada<T> {
 
     public int size(){
         return tamanhoLista;
+    }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        NoDuplo<T> noAuxiliar = primeiroNo;
+
+        //Percorrer a lista
+        for (int i = 0; i < size(); i++){
+            strRetorno += "[No{conteudo: "+ noAuxiliar.getConteudo() + "}] ->";
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        strRetorno += "null";
+        return strRetorno;
     }
 }
--- Estrutura de Dados/Lista-Duplamente-Encadeada/src/Main.java
+++ Estrutura de Dados/Lista-Duplamente-Encadeada/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ListaDuplamenteEncadeada<String> listaDupla = new ListaDuplamenteEncadeada<>();
+
+        listaDupla.add("C1");
+        listaDupla.add("C2");
+        listaDupla.add("C3");
+        listaDupla.add("C4");
+        listaDupla.add("C5");
+        listaDupla.add("C6");
+        listaDupla.add("C7");
+
+
+        System.out.println(listaDupla);
+
+        listaDupla.remove(3);
+        listaDupla.add(3, "99");
+        System.out.println(listaDupla);
+
+        System.out.println(listaDupla.get(3));
+
+        System.out.println(listaDupla.size());
+
+
     }
 }
